ab4e1182cc3bffbe791018be451cdc3b0ce1b234
Azamat <azamat@Rasulovs-MacBook-Air.local>
2024-02-06 14:36:34 +0900

    Set a security HTTP headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorController = require('./controllers/errorController');
@@ -10,10 +11,14 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // GLOBAL MIDDLEWARES
+// Set a security for HTTP requests
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+// Limiting too many requests from same IP
 const limiter = rateLimiter({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -21,9 +26,13 @@ const limiter = rateLimiter({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
   next();
