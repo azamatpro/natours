16184823d861c00f400d684120ca6da5634b7302
azamatpro <azamat.rasulov00@gmail.com>
2024-02-13 17:46:59 +0900

    Completing protecting and authorization of routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -199,3 +199,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 
   next();
 });
+
+
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,11 +4,12 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect); // protects all routes below
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview,
@@ -16,8 +17,14 @@ router
 
 router
   .route('/:id')
-  .patch(reviewController.updateReview)
   .get(reviewController.getReview)
-  .delete(reviewController.deleteReview);
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview,
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview,
+  );
 
 module.exports = router;
