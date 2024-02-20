3971db5f4bf301d49a633cac0868b8f171e975d5
azamatpro <azamat.rasulov00@gmail.com>
2024-02-20 17:41:53 +0900

    Server side rendering, routes and controllers implemantaion

--- app.js
+++ app.js
@@ -1,57 +1,63 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const rateLimiter = require('express-rate-limit');
+const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
-const globalErrorController = require('./controllers/errorController');
-
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
-app.set('view engine', 'pug');
-app.set(path.join(__dirname, 'views'));
 
-// GLOBAL MIDDLEWARES
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// 1) GLOBAL MIDDLEWARES
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
-// Set a security HTTP headers
+
+// Set security HTTP headers
 app.use(helmet());
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-// Limiting too many requests from same IP
-const limiter = rateLimiter({
+
+// Limit requests from same API
+const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'To many requests from the same IP! Please try again in an hour.',
+  message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
 
-// Data sanitization against NoSQL query injections {using symbols, $,., as email}
+// Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 
-// Data sanitization against XSS(preventing html codes in input)
+// Data sanitization against XSS
 app.use(xss());
 
-// Preventing http parameter pollution
+// Prevent parameter pollution
 app.use(
   hpp({
     whitelist: [
       'duration',
-      'ratingsAvarage',
       'ratingsQuantity',
+      'ratingsAverage',
       'maxGroupSize',
       'difficulty',
       'price',
@@ -61,25 +67,20 @@ app.use(
 
 // Test middleware
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// ROUTES
-app.use('/', (req, res) => {
-  res.status(200).render('base');
-});
+// 3) ROUTES
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
-app.use(globalErrorController);
+
+app.use(globalErrorHandler);
+
 module.exports = app;
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -28,43 +28,63 @@ const handleTokenExpiredError = () => {
   return new AppError(message, 401);
 };
 
-const sendErrorDev = (err, res) =>
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-    error: err,
-    stack: err.stack,
-  });
-
-const sendErrorProd = (err, res) => {
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
+const sendErrorDev = (err, req, res) => {
+  if (req.originalUrl.startsWith('/api')) {
+    return res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
+      error: err,
+      stack: err.stack,
     });
-  } else {
+  }
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: err.message,
+  });
+};
+
+const sendErrorProd = (err, req, res) => {
+  if (req.originalUrl.startsWith('/api')) {
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
     // console.error('ERROR:', err);
-    res.status(500).json({
+    return res.status(500).json({
       status: 'error',
       message: 'Something went very wrong!',
     });
   }
+  if (err.isOperational) {
+    return res.status(err.statusCode).render('error', {
+      title: 'Something went wrong!',
+      msg: err.message,
+    });
+  }
+  // console.error('ERROR:', err);
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: 'Please try again later!',
+  });
 };
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
   if (process.env.NODE_ENV === 'development') {
-    return sendErrorDev(err, res);
+    return sendErrorDev(err, req, res);
   }
   if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
+    error.message = err.message;
     if (err.name === 'CastError') error = handleCastErrorDB(error);
     if (err.code === 11000) error = handleDuplicateFieldsError(error);
     if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
     if (err.name === 'JsonWebTokenError') error = handleJWTError();
     if (err.name === 'TokenExpiredError') error = handleTokenExpiredError();
 
-    return sendErrorProd(error, res);
+    return sendErrorProd(error, req, res);
   }
 };
